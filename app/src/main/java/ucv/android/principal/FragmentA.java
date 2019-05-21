7095b4d2422a7185fb8af940bfd51826851c3d1c
Rito <ritorito141516@gmail.com>
2019-05-20 22:29:23 -0500

    lab s8

---
+++ app/src/main/java/ucv/android/principal/FragmentA.java
@@ -0,0 +1,8 @@
+package ucv.android.principal;
+
+import android.support.v4.app.ListFragment;
+
+public class FragmentA extends ListFragment {
+    private final  String[] lista = {"RED","GREEN","BLUE"};
+
+}
