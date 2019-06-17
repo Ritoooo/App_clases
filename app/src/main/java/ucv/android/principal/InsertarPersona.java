a21d239db4d6facb5f9b7613213d4fbdf9d67de6
Ritoooo <ritorito141516@gmail.com>
2019-06-17 10:39:35 -0500

    Creo los primeros activities

---
+++ app/src/main/java/ucv/android/principal/InsertarPersona.java
@@ -0,0 +1,13 @@
+package ucv.android.principal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertarPersona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.insertarpersona);
+    }
+}
